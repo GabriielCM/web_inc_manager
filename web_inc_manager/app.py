1c899bcee54bb630c9a31a9e1d0c19380f325d9d
Gabriel Rodrigues da Silva <mdcarabina@gmail.com>
2025-03-07 09:08:33 -0300

    nova att teste

--- web_inc_manager/app.py
+++ web_inc_manager/app.py
@@ -23,7 +23,7 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-# Adicionar filtro from_json ao Jinja2
+# Adicionar filtro from_json ao Jinja2 tesy
 app.jinja_env.filters['from_json'] = lambda s: json.loads(s)
 
 # Adicionar filtro enumerate ao Jinja2
